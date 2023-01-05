920365ddfed434c88cffd0ea578b505a8012a642
vamsi <saivamsisuru99@gmail.com>
2023-01-05 20:33:12 +0530

    update3

--- innerforloopex1.py
+++ innerforloopex1.py
@@ -1,9 +1,11 @@
 #program for generating 1 to n multiplication table where n is the positive number
 n=int(input("enter the value of n: "))
-for i in range(1,n+1):
-    if(i<1):
-        continue
+if(n<1):
+    print("invalid input")
+else:
+    for i in range(1,n+1):
+            print("="*50)
+            for num in range(1,11):
+                print("{} x {} ={}".format(i,num,num*i))
     else:
-        print("="*50)
-        for num in range(1,11):
-            print("{} x {} ={}".format(i,num,num*i))
+        print("=" * 50)
