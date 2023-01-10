ea29dc93e1cf76aaf8c44af752a2181f108fbe57
vamsi <saivamsisuru99@gmail.com>
2023-01-10 12:39:50 +0530

    01-09-2023

---
+++ approach3ex.py
@@ -0,0 +1,7 @@
+# program for adding two numbers by using funtcions
+def addop(a,b):
+    c=a+b
+    print("{}+{}={}".format(a,b,c))
+a = float(input("enter first value : "))
+b = float(input("enter second value: "))
+addop(a,b)
---
+++ approach4ex.py
@@ -0,0 +1,10 @@
+# program for adding two numbers by using funtcions
+def addop():
+    a=float(input("enter first value : "))
+    b=float(input("enter second value: "))
+    c=a+b
+    return a,b,c
+
+res=addop()
+print("{}".format(res))
+
