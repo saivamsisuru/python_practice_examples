150d73a1dd097abab7c939f57ad868de342fd235
vamsi <saivamsisuru99@gmail.com>
2023-01-25 10:40:56 +0530

    25-01-2023

---
+++ practiceexamples/practice16.py
@@ -0,0 +1,7 @@
+#program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included). 
+squared_num=[x**2 for x in range(1,31)]
+y=squared_num[:5]
+z=squared_num[-5:]
+print("original list=",squared_num)
+print("first 5 elements of their squares=",y)
+print("last 5 elements of their squares=",z)
